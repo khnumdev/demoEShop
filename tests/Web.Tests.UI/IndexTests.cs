d1fe6c0f5ce35a886ea7da3549b33db145c8d4ff
Andrés Pérez <khnumdev@outlook.com>
2019-04-14 20:09:29 +0200

    Added selenium tests

---
+++ tests/Web.Tests.UI/IndexTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Tests.UI
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Diagnostics.CodeAnalysis;
+
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    [TestCategory("UI")]
+    public class IndexTests
+    {
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Title_should_be_catalog()
+        {
+            var driver = TestHelper.GetDriver();
+
+            driver.Navigate().GoToUrl(TestHelper.AppUrl + "/");
+           
+            Assert.IsTrue(driver.Title.Contains("Catalog"), "Verified title of the page");
+
+            driver.Quit();
+        }
+    }
+}
---
+++ tests/Web.Tests.UI/TestHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Web.Tests.UI
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    static class TestHelper
+    {
+        public static string AppUrl { get; set; }
+
+        public static IWebDriver GetDriver()
+        {
+            string browser = "IE";
+            switch (browser)
+            {
+                case "Chrome":
+                    return new ChromeDriver();
+                case "Firefox":
+                    return new FirefoxDriver();
+                case "IE":
+                    return new InternetExplorerDriver();
+                default:
+                    return new ChromeDriver();
+            }
+        }
+    }
+}
